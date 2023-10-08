c0733215944c9f3cec2a6eb0d6cc4898d49a5952
Lesther0212 <murilodrumondlesther@gmail.com>
2023-10-08 20:44:30 -0300

    Md_money

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Background, Container, Logo, AreaInput, Input} from './styles'; 
+
+export default function SignIn(){
+    return(
+        <Background>
+            <Container>
+                <Logo source={require('../../assets/Logo.png')}/>
+
+                <AreaInput>
+                    <Input placeholder="Seu email"/>
+                </AreaInput>
+            </Container>
+        </Background>
+    );
+}
+
